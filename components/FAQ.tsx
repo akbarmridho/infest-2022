b680ceccede9aa45007865d3237230ab547a7e94
Akbar Maulana R <akbarmaulanaridho1@gmail.com>
2022-07-21 17:30:44 +0700

    faq active state

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,16 +1,46 @@
 import { Component } from "react";
 
-export default class FAQ extends Component {
+interface Props {
+    question: string,
+    answer: string
+}
+
+interface State {
+    active: boolean
+}
+
+export default class FAQ extends Component<Props, State> {
+    constructor(props: Props | Readonly<Props>) {
+        super(props)
+        this.state = {active: false}
+        this.handleToggleClick = this.handleToggleClick.bind(this)
+    }
+
+    handleToggleClick() {
+        this.setState(state => ({active: !state.active}))
+    }
+
     render() {
         return (
             <div className="m-4">
-                <div className='w-full rounded-[16px] px-5 py-6 bg-tyellow-100 flex flex-row items-center'>
-                <div className='text-lg font-semibold flex-grow'>
-                    Lorem ipsum dos color sit amet
-                </div>
-                <div>
-                    <button className="rounded-full float-right bg-gray-400 w-8 h-8">&#10005;</button>
-                </div>
+                <div className={`w-full rounded-[16px] px-5 py-6 flex flex-row items-top ${this.state.active ? "bg-tyellow-100" : "bg-white"}`}>
+                    <div className="flex-grow">
+                        <div className='text-lg font-semibold'>
+                            {this.props.question}
+                        </div>
+                        {
+                            this.state.active &&
+                                <div className="mt-4">
+                                    {this.props.answer}
+                                </div>
+                        }
+                    </div>
+                    <div className='ml-8'>
+                        <button 
+                            className="rounded-full float-right w-8 h-8 bg-[rgba(255,255,255,0.45)] font-bold text-gray-900"
+                            dangerouslySetInnerHTML={{ __html: this.state.active ? '&#8211;' : '&#10005;'}}
+                            onClick={this.handleToggleClick}></button>
+                    </div>
                 </div>
             </div>
         )
--- components/FAQDeck.tsx
+++ components/FAQDeck.tsx
@@ -6,10 +6,9 @@ export default class FAQDeck extends Component {
         return (
             <div className='flex justify-center'>
                 <div className='bg-gray-500 w-full mx-16 my-8 max-w-screen-md'>
-                    <FAQ />
-                    <FAQ />
-                    <FAQ />
-                    <FAQ />
+                    <FAQ question='lorem ipsum dos color sit amet' answer='this is some fucking answer loooong answer as;ldkjasdmsaoi asldjaslkdas asldkjashdkljas aslkjdas askljdhasljkd aslkjdhas'/>
+                    <FAQ question='lorem ipsum dos color sit amet' answer='this is some fucking answer'/>
+                    <FAQ question='lorem ipsum dos color sit amet' answer='this is some fucking answer'/>
                 </div> 
             </div>
         )
