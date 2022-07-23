f2dad696013a3ef235e2f6464b2efb13572765bc
maikeljh <mjh191003@gmail.com>
2022-07-24 02:41:59 +0700

    fix footer css temp

--- components/Teaser.tsx
+++ components/Teaser.tsx
@@ -1,6 +1,6 @@
 const Teaser = () => {
     return (
-        <div className='flex justify-center md:mt-60 mt-96'>
+        <div className='flex justify-center mt-60'>
             <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"
             className="lg:w-[816px] lg:h-[462px] md:w-[608px] md:h-[431px] w-[408px] h-[231px]"
             title="YouTube video player" frameBorder="0" 
