dce048be5c345bd7de57f4ba431c8e4c37cab257
Akbar Maulana R <akbarmaulanaridho1@gmail.com>
2022-07-28 10:17:46 +0700

    add hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,25 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import '../components/Footer.css'
 import { GoogleAnalytics, usePageViews } from 'nextjs-google-analytics'
+import { useEffect } from 'react'
+import { hotjar } from 'react-hotjar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   usePageViews()
+
+  useEffect(() => {
+    try {
+
+      if (process.env.NEXT_PUBLIC_HJID && process.env.NEXT_PUBLIC_HJSV) {
+          const HJID = parseInt(process.env.NEXT_PUBLIC_HJID)
+          const HJSV = parseInt(process.env.NEXT_PUBLIC_HJSV)
+          hotjar.initialize(HJID, HJSV)
+      }
+      
+    } catch (e) {
+      console.log("Skipping hotjar")
+    }
+  }, [])
   
   return (
     <Layout>
