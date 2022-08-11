2385864e238721656367dbab02e6e81e3ad031c9
maikeljh <mjh191003@gmail.com>
2022-08-12 02:39:54 +0700

    add multiple deck speaker

--- components/SpeakerDeck.tsx
+++ components/SpeakerDeck.tsx
@@ -1,28 +1,32 @@
 import Speaker, { SpeakerProps } from "./Speaker";
-
+import { useState } from "react";
+import FAQButton from "./FAQButton";
 interface Props {
-    speakers: Omit<SpeakerProps, 'pos'>[]
+    speakers : {
+        [key: string] : SpeakerProps[]
+    }
 }
 
-// Todo: tombol seperti pada FAQMultibleDeck untuk menampilkan pembicara pada day yang berbeda
-
-
 const SpeakerDeck = ({ speakers }: Props) => {
+    const [activeItems, setActiveItems] = useState<string>(Object.keys(speakers)[0])
     return (
         <div className='flex justify-center'>
             <div className="w-full mx max-w-screen-md">
                 <h2 className="h2 text-center text-gradient mb-4 lg:mb-6">Pembicara Kami</h2>
-                <div className='flex flex-col'>
+                <div className="flex justify-center flex-wrap">
                     {
-                        speakers.length > 0 ?
-                            speakers.map((speaker, i) => {
-                                return (
-                                    <Speaker name={speaker.name} topic={speaker.topic} imageUrl={speaker.imageUrl} pos={ i % 2 === 0 ? 'left' : 'right'} key={i}/>
-                                )
-                            })
-                        : <h3 className="text-center text-gray-100 text-3xl font-tommy my-4">Akan diumumkan di kemudian hari</h3>
+                        Object.keys(speakers).map((key, i) => {
+                            return <FAQButton key={i} name={key} onClick={() => setActiveItems(key)} currentActive={activeItems}/>
+                        })
                     }
                 </div>
+                <div className="mt-6">
+                    {
+                        speakers[activeItems].map((speaker, i) => {
+                            return <Speaker key={`${i}-${activeItems}`} name={speaker.name} topic={speaker.topic} imageUrl={speaker.imageUrl} pos={ i % 2 === 0 ? 'left' : 'right'}/>
+                        })
+                    }
+                </div> 
             </div>
         </div>
     )
