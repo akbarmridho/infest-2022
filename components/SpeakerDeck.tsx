f4d65f377151b69386790c99266ce1d8f72418de
Akbar Maulana R <akbarmaulanaridho1@gmail.com>
2022-08-15 11:52:35 +0700

    fix speaker deck

--- components/SpeakerDeck.tsx
+++ components/SpeakerDeck.tsx
@@ -2,34 +2,53 @@ import Speaker, { SpeakerProps } from "./Speaker";
 import { useState } from "react";
 import FAQButton from "./FAQButton";
 interface Props {
-    speakers : {
-        [key: string] : SpeakerProps[]
-    }
+  speakers: {
+    [day: string]: {
+      name: string;
+      topic: string;
+      imageUrl: string;
+    }[];
+  };
 }
 
 const SpeakerDeck = ({ speakers }: Props) => {
-    const [activeItems, setActiveItems] = useState<string>(Object.keys(speakers)[0])
-    return (
-        <div className='flex justify-center'>
-            <div className="w-full mx max-w-screen-md">
-                <h2 className="h2 text-center text-gradient mb-4 lg:mb-6">Pembicara Kami</h2>
-                <div className="flex justify-center flex-wrap">
-                    {
-                        Object.keys(speakers).map((key, i) => {
-                            return <FAQButton key={i} name={key} onClick={() => setActiveItems(key)} currentActive={activeItems}/>
-                        })
-                    }
-                </div>
-                <div className="mt-6">
-                    {
-                        speakers[activeItems].map((speaker, i) => {
-                            return <Speaker key={`${i}-${activeItems}`} name={speaker.name} topic={speaker.topic} imageUrl={speaker.imageUrl} pos={ i % 2 === 0 ? 'left' : 'right'}/>
-                        })
-                    }
-                </div> 
-            </div>
+  const [activeItems, setActiveItems] = useState<string>(
+    Object.keys(speakers)[0]
+  );
+  return (
+    <div className="flex justify-center">
+      <div className="w-full mx max-w-screen-md">
+        <h2 className="h2 text-center text-gradient mb-4 lg:mb-6">
+          Pembicara Kami
+        </h2>
+        <div className="flex justify-center flex-wrap">
+          {Object.keys(speakers).map((key, i) => {
+            return (
+              <FAQButton
+                key={i}
+                name={key}
+                onClick={() => setActiveItems(key)}
+                currentActive={activeItems}
+              />
+            );
+          })}
         </div>
-    )
-}
+        <div className="mt-6">
+          {speakers[activeItems].map((speaker, i) => {
+            return (
+              <Speaker
+                key={`${i}-${activeItems}`}
+                name={speaker.name}
+                topic={speaker.topic}
+                imageUrl={speaker.imageUrl}
+                pos={i % 2 === 0 ? "left" : "right"}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SpeakerDeck
+export default SpeakerDeck;
